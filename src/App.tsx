6791e5afdd002c77cc5702646dc4fac3b3c92c5e
Suka <mateodefalco1@gmail.com>
2023-08-24 15:59:53 -0300

    Card creation and ui package instalation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import "./App.css"
-
 function App() {
-	return <main></main>
+	return (
+		<main className="flex flex-col justify-center items-center w-screen h-screen">
+			<div className="card w-96 bg-base-100 shadow-xl">
+				<figure>
+					<img
+						src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+						alt="Shoes"
+					/>
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">Shoes!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div className="card-actions justify-end">
+						<button className="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default App
