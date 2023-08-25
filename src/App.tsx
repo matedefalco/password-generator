a18161abdf93e3ead056cf7db8a73ebc64c2ab01
Suka <mateodefalco1@gmail.com>
2023-08-25 11:53:16 -0300

    useRef fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import PasswordGenerator from "./components/PasswordGenerator"
 import GeneratedPassword from "./components/GeneratedPassword"
+import { useRef } from "react"
 
-function App() {
+const App = () => {
+	const generatedPasswordRef = useRef<string | null>(null)
+
+	const handlePasswordGenerated = (password: string) => {
+		console.log("Suka ~ file: App.tsx:9 ~ password:", password)
+		generatedPasswordRef.current = password
+	}
+
 	return (
 		<main className="flex flex-col justify-center items-center w-screen h-screen gap-8">
-			<h1 className="text-2xl text-slate-400">Password generator</h1>
-			<GeneratedPassword />
-			<PasswordGenerator />
+			<h1 className="text-2xl text-slate-500">Password generator</h1>
+			<GeneratedPassword ref={generatedPasswordRef} />
+			<PasswordGenerator onPasswordGenerated={handlePasswordGenerated} />
 		</main>
 	)
 }
