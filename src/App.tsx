7f51d770b1fea80e99d37dd00533e438a36bd475
Suka <mateodefalco1@gmail.com>
2023-08-25 13:12:30 -0300

    References

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from "react"
 import PasswordGenerator from "./components/PasswordGenerator"
 import GeneratedPassword from "./components/GeneratedPassword"
 
+const imgUrls = [
+	"https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png",
+	"https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png",
+	"https://raw.githubusercontent.com/saadeghi/daisyui/master/src/docs/static/images/daisyui-logo/favicon-192.png",
+]
+
 const App = () => {
 	const [generatedPassword, setGeneratedPassword] = useState<string>("")
 
@@ -13,7 +20,21 @@ const App = () => {
 		<main className="flex flex-col justify-center items-center w-screen h-screen gap-8">
 			<h1 className="text-2xl text-slate-500">Password generator</h1>
 			<GeneratedPassword generatedPassword={generatedPassword} />
-			<PasswordGenerator onPasswordGenerated={handlePasswordGenerated} />
+			<PasswordGenerator onPasswordGenerated={handlePasswordGenerated} />{" "}
+			<footer className="flex flex-col gap-2 items-center">
+				<p>Made with:</p>
+				<ul className="flex gap-2">
+					{imgUrls.map((url, index) => (
+						<li key={index} className="references">
+							<img
+								alt={`Image ${index}`}
+								className="w-8 h-8 object-contain"
+								src={url}
+							/>
+						</li>
+					))}
+				</ul>
+			</footer>
 		</main>
 	)
 }
