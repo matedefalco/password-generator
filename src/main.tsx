a71bab4e22c5c507fad5fb268d44d87caed326a7
Suka <mateodefalco1@gmail.com>
2023-08-29 15:33:20 -0300

    Clerk Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	RedirectToSignIn,
+} from "@clerk/clerk-react"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+	throw new Error("Missing Publishable key")
+}
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ClerkProvider publishableKey={clerkPubKey}>
+			<SignedOut>
+				<RedirectToSignIn />
+			</SignedOut>
+			<SignedIn>
+				<App />
+			</SignedIn>
+		</ClerkProvider>
+	</React.StrictMode>
 )
